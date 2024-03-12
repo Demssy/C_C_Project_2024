c030a8dabceb6580ae66129316824c1684b8a2d4
vanya <vanya@DESKTOP-ON17JGP>
2024-03-12 22:18:16 +0200

    edintity

--- C_C_Proj.DataAccess/Data/ApplicationDbContext.cs
+++ C_C_Proj.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿
 using C_C_Proj_WebStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace C_C_Proj_WebStore.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
